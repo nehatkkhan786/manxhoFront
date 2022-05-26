8ede1dc1b7bfc1744dfff3c1cac2374acb94d7c6
adarsh <adramelech.psd@gmail.com>
2022-05-26 09:14:44 +0530

    fix : adding single cartitem multiple times

--- src/components/reducers/product/ProductReducer.jsx
+++ src/components/reducers/product/ProductReducer.jsx
@@ -1,31 +1,41 @@
-const ProductReducer = (state, action) =>{
-    switch(action.type){
-        case 'GET_ALL_PRODUCTS':
-            return {
-                ...state, 
-                products : action.payload,
-                loading : false
-            }
-        case 'GET_ALL_CATEGORIES':
-            return {
-                ...state,
-                categories : action.payload,
-                loading : false
-            }
+const ProductReducer = (state, action) => {
+	switch (action.type) {
+		case "GET_ALL_PRODUCTS":
+			return {
+				...state,
+				products: action.payload,
+				loading: false,
+			};
+		case "GET_ALL_CATEGORIES":
+			return {
+				...state,
+				categories: action.payload,
+				loading: false,
+			};
 
-        case 'ADD_TO_CART':
-            const item = action.payload
-            const existItem = state.cartItems.find((x)=>x._id === item._id)
-                return {
-                    ...state,
-                    cartItems:[...state.cartItems, item]
-                }
+		case "ADD_TO_CART":
+			const item = action.payload;
+			const existItem = state.cartItems.find((x) => x._id === item._id);
+			if (existItem) {
+				return {
+					...state,
+					cartItems: state.cartItems.map((item) => {
+						if (item._id === existItem._id) {
+							return { ...item, qty: item.qty + 1 };
+						}
+						return item;
+					}),
+				};
+			} else {
+				return {
+					...state,
+					cartItems: [...state.cartItems, item],
+				};
+			}
 
-
-            
-        default:
-            return state
-    }
-}
+		default:
+			return state;
+	}
+};
 
 export default ProductReducer;
